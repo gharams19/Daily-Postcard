f9e0aaeecdd25373242bf3f6d7417d1643611982
Glitch (assets-lib) <none>
2020-04-10 15:37:27 +0000

    🐀🍐 Checkpoint ./public/script.js:14044413/393 ./public/style.css:14044413/151

--- public/script.js
+++ public/script.js
@@ -41,7 +41,17 @@ colorBoxes.forEach((b, i) => {
   });
   
   b.addEventListener('mouseover', () => {
-    b.style.border 
+    b.style.border = '1px dashed black';
+    document.querySelector('.postcard').style.backgroundColor = colors[i];
+  });
+  b.addEventListener('mouseout', () => {
+    if(b != currentColor) {
+      b.style.border = 'none';
+      document.querySelector('.postcard').style.backgroundColor = currentColor.style.backgroundColor;
+    } else {
+      b.style.border = '1px solid black';
+    }
   })
+  
   
 })
