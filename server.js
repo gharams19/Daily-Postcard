3642b45ea6f6e8a14efd39271a9d811279ae81e4
Glitch (assets-lib) <none>
2020-04-10 05:16:24 +0000

    ⚾️🌁 Checkpoint ./public/index.html:14044413/31 ./server.js:14044413/228

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 var express = require('express');
 var app = express();
 var assets = require('./assets');
+const multer = require('multer');
+const upload = multer({dest: __dirname + '/uploads'});
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -15,6 +17,13 @@ app.use("/assets", assets);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/public/index.html');
 });
+
+app.post('/upload', upload.single('photo'), (req, res) => {
+  if(req.file) {
+    res.json(req.file);
+  }
+  else throw 'error';
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
