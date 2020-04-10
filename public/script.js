a4e46220a0d8e1dd3fc6d19c70cd5957b517c412
Glitch (assets-lib) <none>
2020-04-10 14:50:34 +0000

    🍅💝 Checkpoint ./public/index.html:14044413/641 ./public/script.js:14044413/212

--- public/script.js
+++ public/script.js
@@ -1,5 +1,17 @@
 "use strict";
 
+const colors = [
+  '#e6e2cf',
+  '#B5A6AB',
+  '#ECCFCF',
+  '#A6A5B5',
+  '#BAB9B5',
+  '#c9cbb3',
+  '#dbcaac',
+  '#bbc9ca',
+  '#eceeeb'
+]
+
 document.querySelectorAll('#fonts input').forEach((i) => {
   i.addEventListener('change', () => {
     console.log(i);
