b713460598171bf04af0ecd4075326e47147997d
Glitch (assets-lib) <none>
2020-04-10 04:42:03 +0000

    🏟🍘 Checkpoint ./public/script.js:14044413/183 ./public/index.html:14044413/1132 ./public/style.css:14044413/62

--- public/script.js
+++ public/script.js
@@ -2,6 +2,10 @@
 
 document.querySelectorAll('#fonts input').forEach((i) => {
   i.addEventListener('change', () => {
-    
+    console.log(i);
+    if(i.checked) {
+      console.log('checked');
+      document.querySelector('#message').className = i.value;
+    }
   })
 })
