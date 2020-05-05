348af95aa948827f17d6cd1576d2817c3bf3040a
Glitch (postcard-app) <none>
2020-05-05 22:11:30 +0000

    🕌🚩 Checkpoint ./server.js:13956730/5901

--- server.js
+++ server.js
@@ -2,79 +2,97 @@
 // where your node app starts
 
 // include modules
-const express = require('express');
+const express = require("express");
 
-const multer = require('multer');
-const bodyParser = require('body-parser');
+const multer = require("multer");
+const bodyParser = require("body-parser");
 const sql = require("sqlite3").verbose();
-const fs = require('fs');
+const fs = require("fs");
 
+const postcardDB = new sql.Database("postcard.db");
 
-let storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, __dirname+'/images')    
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname)
+let cmd =
+  " SELECT name FROM sqlite_master WHERE type = 'table' AND name = 'PostcardTable' ";
+postcardDB.get(cmd, function(err, val) {
+  console.log(err, val);
+  if (val == undefined) {
+    console.log("No database file - creating one");
+    createPostcardDB();
+  } else {
+    console.log("Database file found");
   }
-})
-// let upload = multer({dest: __dirname+"/assets"});
-let upload = multer({storage: storage});
+});
 
+function createPostcardDB() {
+  const cmd = "CREATE TABLE PostcardTable ( rowIdNum INTEGER PRIMARY KEY, message TEXT, )"
+}
+let storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, __dirname + "/images");
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+// let upload = multer({dest: __dirname+"/assets"});
+let upload = multer({ storage: storage });
 
 // begin constructing the server pipeline
 const app = express();
 
-
-
 // Serve static files out of public directory
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Also serve static files out of /images
-app.use("/images",express.static('images'));
+app.use("/images", express.static("images"));
 
 // Handle GET request to base URL with no other route specified
 // by sending creator.html, the main page of the app
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/public/creator.html');
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/public/creator.html");
 });
 
 // Next, the the two POST AJAX queries
 
-// Handle a post request to upload an image. 
-app.post('/upload', upload.single('newImage'), function (request, response) {
-  console.log("Recieved",request.file.originalname,request.file.size,"bytes")
-  if(request.file) {
-    // file is automatically stored in /images, 
-    // even though we can't see it. 
+// Handle a post request to upload an image.
+app.post("/upload", upload.single("newImage"), function(request, response) {
+  console.log(
+    "Recieved",
+    request.file.originalname,
+    request.file.size,
+    "bytes"
+  );
+  if (request.file) {
+    // file is automatically stored in /images,
+    // even though we can't see it.
     // We set this up when configuring multer
-    response.end("recieved "+request.file.originalname);
-  }
-  else throw 'error';
+    response.end("recieved " + request.file.originalname);
+  } else throw "error";
 });
-
 
 // Handle a post request containing JSON
 app.use(bodyParser.json());
 // gets JSON data into req.body
-app.post('/saveDisplay', function (req, res) {
+app.post("/saveDisplay", function(req, res) {
   console.log(req.body);
   // write the JSON into postcardData.json
-  fs.writeFile(__dirname + '/public/postcardData.json', JSON.stringify(req.body), (err) => {
-    if(err) {
-      res.status(404).send('postcard not saved');
-    } else {
-      res.send("All well")
+  fs.writeFile(
+    __dirname + "/public/postcardData.json",
+    JSON.stringify(req.body),
+    err => {
+      if (err) {
+        res.status(404).send("postcard not saved");
+      } else {
+        res.send("All well");
+      }
     }
-  })
-  
+  );
 });
 
-
-// The GET AJAX query is handled by the static server, since the 
+// The GET AJAX query is handled by the static server, since the
 // file postcardData.json is stored in /public
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
 });
